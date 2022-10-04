6ae23531eeaf899cc4374c18941523ac41d5cbd5
MBabattya <105806841+MBabattya@users.noreply.github.com>
2022-10-04 09:13:42 -0700

    Create constant.sol

---
+++ Solidity/Practice/constant.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+   pragma solidity 0.8.0;
+
+contract Constant {
+
+    address public constant onwer=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+   // If we want to keep a state veriable fixed then we use constant keyword 
+   // when making the state veriable constant, you have to use pure keyword while returning it
+   // if you use constant keyword you have to imput the value in that line
+
+}
